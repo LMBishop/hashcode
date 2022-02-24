07f94cf0236e48003028ac6c58e07ce1c672b43e
RogerGabeller-ml <gcb163@student.bham.ac.uk>
2022-02-24 18:26:02 +0000

    ReadFile.java

---
+++ src/ReadFile.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.Scanner;
+
+public class ReadFile {
+    public static String[][] readFiles(String name) {
+        File f = new File(name);
+        Scanner sc = new Scanner(f);
+        while (sc.hasNextLine()) {
+            String data
+        }
+    }
+}
